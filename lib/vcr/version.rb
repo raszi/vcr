58d027158ebe5c8fc740f0cec91860c7f65015ed
KARASZI István <github@spam.raszi.hu>
2015-12-02 14:58:30 +0100

    Bump up the version

--- lib/vcr/version.rb
+++ lib/vcr/version.rb
@@ -10,7 +10,7 @@ module VCR
   #   * `parts` [Array<Integer>] List of the version parts.
   def version
     @version ||= begin
-      string = '3.0.0'
+      string = '3.0.1.wp0'
 
       def string.parts
         split('.').map { |p| p.to_i }
